e9b14e00091dfb64bb35ca3e0c4b66ff5dbb4f46
tewtew023 <abcdtivolitwin@gmail.com>
2020-10-14 16:12:02 +0700

    initial Webview Exatendsion for VScode

---
+++ cat-coding/out/extension.js
@@ -0,0 +1,30 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const vscode = require("vscode");
+function activate(context) {
+    context.subscriptions.push(vscode.commands.registerCommand('catCoding.start', () => {
+        // Create and show a new webview
+        const panel = vscode.window.createWebviewPanel('catCoding', // Identifies the type of the webview. Used internally
+        'Cat Coding', // Title of the panel displayed to the user
+        vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+        {} // Webview options. More on these later.
+        ); // And set its HTML content
+        panel.webview.html = getWebviewContent();
+    }));
+}
+exports.activate = activate;
+function getWebviewContent() {
+    return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+	  <meta charset="UTF-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	  <title>Cat Coding</title>
+  </head>
+  <body>
+	  <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
+	  พ่อมองขึ้นมาจะเสียใจแค่ไหน
+  </body>
+  </html>`;
+}
+//# sourceMappingURL=extension.js.map
