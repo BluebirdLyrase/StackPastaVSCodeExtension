4fb0783eb4a7d3bea1e03a7ccb637ad07839e5d0
BluebirdLyrase <pisitkeawfung@gmail.com>
2020-11-14 12:34:34 +0700

    Test API

--- vscode-express-example-master/view/js/index.js
+++ vscode-express-example-master/view/js/index.js
@@ -1,1 +1,23 @@
 
+function searchformstack(){
+
+    console.log("searchformstack")
+
+    pageSize = "40"
+    order = "asc"
+    sort = "relevance"
+    intitle = "Eclipse"
+    tagContent = ""
+    site = "stackoverflow"
+
+    var url = "https://api.stackexchange.com/2.2/search/advanced?page=1&pagesize=" + this.pageSize
+        + "&order=" + this.order + "&sort=" + this.sort + "&q=" + this.intitle + "&accepted=True" + tagContent
+        + "&site=" + this.site + "&filter=!4(L6lo9D9J9Y3508i";
+
+    $.get(url, function (data, status) {
+
+        $("#searchResult").append(JSON.stringify(data));
+        console.log(data)
+
+    });
+}
